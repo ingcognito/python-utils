5f90bbe02031ef65a865c1bd1364edb51c8e7cb7
noah-ing <ing.noah@gmail.com>
2020-09-14 17:29:45 -0400

    qrcode scanner

--- qr-code/qr-code.py
+++ qr-code/qr-code.py
@@ -1,6 +1,8 @@
 import qrcode
+from datetime import date
 
 s = input("Input a URL: ")
+
 if s=="":
     print("Please enter a URL")
     exit(0)
@@ -13,7 +15,7 @@ qr = qrcode.QRCode(
 )
 qr.add_data(s)
 qr.make()
-img = qr.make_image(fill_color="red", back_color="#23dda0")
-img.save('./qrcode-' + s + '.png')
-print(f"QR code saved as qrcode{s}.png")
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('./qrcode.png')
+print("QR code has been saved as PNG.")
 
