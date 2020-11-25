68bf5d10df0c1ecd4f9ea00d7ea21795e7908f5d
noah-ing <ing.noah@gmail.com>
2020-11-25 16:31:46 -0500

    utility oop script template

---
+++ utility-template.py
@@ -0,0 +1,36 @@
+import logging
+import argparse
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='Parsin some args')
+    parser.add_argument('--name', required=True, type=str,
+                        help='name of person')
+    parser.add_argument('--age', required=True, type=str,
+                        help='age of person')
+    args = parser.parse_args()
+    return args
+
+
+class Utility:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def hello(self):
+        name = self.name
+        age = self.age
+        print(f" Hello my name is {name}, I am {age} years old")
+
+
+def main():
+    args = arg_parse()
+    name = args.name
+    age = args.age
+
+    utility = Utility(name=name, age=age)
+    utility.hello()
+
+if __name__ == '__main__':
+    main()
+
